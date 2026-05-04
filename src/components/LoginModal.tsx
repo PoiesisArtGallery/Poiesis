d513863f05b1249960d7f6ff6960afe9c13db640
PoiesisArtGallery <poiesis.art.gallery.pag@gmail.com>
2026-05-05 01:51:14 +0530

    small update

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -37,7 +37,7 @@ export default function LoginModal({ open, setOpen }: any) {
   email,
   password,
   options: {
-    emailRedirectTo: "http://localhost:3000"
+    emailRedirectTo: "https://www.poiesisartgallery.com/login"
   }
 })
       
